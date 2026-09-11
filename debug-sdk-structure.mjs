e0b87d91f73b6196b2f91058c03a70759fbd28aa
RocAgent <rocagent@owner.local>
2026-09-12 02:29:04 +0700

    Clean: remove remaining debug/test files, update .gitignore

--- debug-sdk-structure.mjs
+++
@@ -1,9 +0,0 @@
-import * as scw from 'scaleway';
-console.log('=== SDK Exports ===');
-console.log(Object.keys(scw));
-console.log('---');
-
-// Check what's in the default export
-if (scw.default) {
-  console.log('default export:', Object.keys(scw.default));
-}
--- inspect-sdk.cjs
+++
@@ -1,5 +0,0 @@
-// Inspect Scaleway SDK exports
-const scw = require('@scaleway/sdk');
-
-console.log("=== Scaleway SDK Modules ===");
-console.log(Object.keys(scw).join('\n'));
