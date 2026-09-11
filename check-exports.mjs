829ea82d1f6ab167689c14abc6b6b1c22faf294f
RocAgent <rocagent@owner.local>
2026-09-12 02:29:22 +0700

    Clean: remove test-scaleway folder, update .gitignore

--- check-exports.mjs
+++
@@ -1,9 +0,0 @@
-import * as account from "@scaleway/sdk-account";
-import * as rdb from "@scaleway/sdk-rdb";
-import * as func from "@scaleway/sdk-function";
-import * as billing from "@scaleway/sdk-billing";
-
-console.log("Account exports:", Object.keys(account));
-console.log("RDB exports:", Object.keys(rdb));
-console.log("Function exports:", Object.keys(func));
-console.log("Billing exports:", Object.keys(billing));
